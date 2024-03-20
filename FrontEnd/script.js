4d100a705d3beb29f3c749fd0a0aa06b1d9f9b28
mnlazs <mnl.azs@gmail.com>
2024-03-20 12:22:53 -0500

    updated images files

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,15 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const chatbotButton = document.getElementById('chatbot-button');
   const chatbotPopup = document.getElementById('chatbot-popup');
 
-  // Muestra la ventana emergente cuando el cursor entra en el área
-  chatbotPopup.addEventListener('mouseenter', () => {
-      chatbotPopup.style.opacity = '1';
-      chatbotPopup.style.visibility = 'visible';
+  // Muestra la ventana emergente cuando el cursor entra en el área del botón
+  chatbotButton.addEventListener('mouseenter', () => {
+    chatbotPopup.style.opacity = '1';
+    chatbotPopup.style.visibility = 'visible';
   });
 
-  // Oculta la ventana emergente cuando el cursor sale del área
+  // Oculta la ventana emergente cuando el cursor sale del área del botón y la ventana emergente
+  chatbotButton.addEventListener('mouseleave', () => {
+    chatbotPopup.style.opacity = '0';
+    chatbotPopup.style.visibility = 'hidden';
+  });
+
+  chatbotPopup.addEventListener('mouseenter', () => {
+    chatbotPopup.style.opacity = '1';
+    chatbotPopup.style.visibility = 'visible';
+  });
+
   chatbotPopup.addEventListener('mouseleave', () => {
-      chatbotPopup.style.opacity = '0';
-      chatbotPopup.style.visibility = 'hidden';
+    chatbotPopup.style.opacity = '0';
+    chatbotPopup.style.visibility = 'hidden';
   });
 });
