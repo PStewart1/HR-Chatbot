80d4bbbd74cbc5e0e571037097ee04e06d86c875
mnlazs <mnl.azs@gmail.com>
2024-04-04 14:41:24 -0500

    updated

--- FrontEnd/mychatbot/src/components/HeaderBar/HeaderBar.js
+++ FrontEnd/mychatbot/src/components/HeaderBar/HeaderBar.js
@@ -6,7 +6,6 @@ import logo from '../../assets/images/EMSlogo2-removebg.png';
 const HeaderBar = () => {
   return (
     <div className="header-bar">
-      <span>Chat with Us 💬</span>
       <img src={logo} alt="Company Logo" className="header-logo" />
     </div>
   );
