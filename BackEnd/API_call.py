26f078c1b57b4c951659a503595e016bc801cdb2
PStewart1 <paul.stewart@atlasschool.com>
2024-04-05 20:09:34 -0500

    added readme with instructions

--- BackEnd/API_call.py
+++ BackEnd/API_call.py
@@ -5,17 +5,9 @@ from extract_information import query_data
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-# create a .env file and add the DB_CONNECTION and COHERE_API_KEY variables
-
-# install all the dependencies in the dockerfile
+# install all the dependencies in install-python-lin.sh
 # install flask and start the api
 # flask --app API_call.py run
-
-# OR
-# start the docker container. instructions in the docker file
-# install httpie
-# open another terminal and run the http requests
-# http POST localhost:5000 query="what is the name of this company?"
 
 @app.route('/', methods=['POST', 'GET'])
 def chat_data():
@@ -28,10 +20,7 @@ def chat_data():
         source_documents = response[1]
         return response[0]
     else:
-        return "Hello World"
-
-
-
+        return "Hello! What can I help you with today?"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
