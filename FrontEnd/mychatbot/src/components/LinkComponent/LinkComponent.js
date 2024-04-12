3d05e22b9c8368a5b36369bbc28dff9abcf45341
PStewart1 <paul.stewart@atlasschool.com>
2024-04-11 21:17:45 -0500

    adde dae's changes

--- FrontEnd/mychatbot/src/components/LinkComponent/LinkComponent.js
+++ FrontEnd/mychatbot/src/components/LinkComponent/LinkComponent.js
@@ -7,13 +7,13 @@ const LinkComponent = () => {
     <div className="link-container">
       <h2>Resources</h2>
       <div className="link-list">
-        <a href="https://docs.google.com/document/d/1c4hNRwKZSZ8Cv0JGvk4ZYy9dCgDZo5D28x1sLsSohjA/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
+        <a href="https://drive.google.com/file/d/1oIElusoelLIrOxubNZaWF9NvNfzTUol8/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
           New Client Onboarding
         </a>
-        <a href="https://docs.google.com/document/d/165tkapgKh4f_OX_-OgG2PdN5gQ0Suu2-fGf_RzwgMsw/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
+        <a href="https://drive.google.com/file/d/1PlTemjOIkRN5KsfdyYb3ka9o8LdYqRTD/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
           Employee Handbook
         </a>
-        <a href="https://docs.google.com/document/d/1iWD45tn8DlCR3iG71RMfRWuhRaYQsxRipziUzZFP-xM/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
+        <a href="https://drive.google.com/file/d/1CTmQRzruRisCQU3xj3B5e1vBVnqZRYG3/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
           Sample FAQ
         </a>
         <a href="https://calendly.com/krystalspeed/discoverycall" target="_blank" rel="noopener noreferrer">
